2cc924e0fe51056ae2ccb65d91d28d2292170435
Enver Olku <olkuenver@gmail.com>
2026-05-03 19:13:23 +0300

    chore: remove obsolete scripts, temporary audit files, and unused local static assets prior to production deployment

--- test_enum.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy.schema import CreateTable
-from sqlalchemy.dialects import postgresql
-from evr_bot.models import User
-
-print(CreateTable(User.__table__).compile(dialect=postgresql.dialect()))
